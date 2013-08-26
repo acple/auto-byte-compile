914207b831e85d5393742575130f1773b2907ebf
acple <silentsphere110@gmail.com>
2013-08-27 08:46:28 +0900

    WIP

--- auto-byte-compile.el
+++ auto-byte-compile.el
@@ -1,3 +1,6 @@
+;;; auto-byte-compile.el --- auto byte compile
+
+;;; Code:
 
 (require 'bytecomp)
 
@@ -6,8 +9,6 @@
   :type 'boolean
   :group 'auto-byte-compile)
 
-(defvar auto-byte-compile-process-buffer " *auto-byte-compile*")
-(defvar auto-byte-compile-log-buffer "*auto-byte-compile*")
 (defvar auto-byte-compile-buffer "*auto-byte-compile*")
 
 (defun auto-byte-compile ()
@@ -16,26 +17,25 @@
          (not no-byte-compile)
          (or auto-byte-compile-no-ask
              (file-exists-p (byte-compile-dest-file file))
-             (y-or-n-p (format "Byte compile %s now?" (buffer-name))))
+             (y-or-n-p (format "Compile \"%s\" ?" (buffer-name))))
          (auto-byte-compile-run file))))
 
 (defun auto-byte-compile-run (file)
-  (let ((inhibit-quit t)
-        process)
-    (setq process (start-process
+  (let* ((inhibit-quit t)
+         (process-connection-type nil)
+         (process (start-process
                    (file-relative-name file)
                    (get-buffer-create auto-byte-compile-buffer)
                    "emacs" "-batch" "-eval"
                    "(batch-byte-compile)"
-                   file))
-    (set-process-sentinel process #'auto-byte-compile-process-sentinel)
-    (set-process-filter process #'auto-byte-compile-process-filter)
-    ;; (set-process-query-on-exit-flag process nil)
-    ))
+                   file)))
+    (set-process-sentinel process 'auto-byte-compile-process-sentinel)
+    (set-process-filter process 'auto-byte-compile-process-filter)
+    (set-process-query-on-exit-flag process nil)))
 
 (defun auto-byte-compile-process-sentinel (process state)
   (if (string= state "finished\n")
-      (message "auto-byte-compile: \"%s\" is compiled successfully."
+      (message "auto-byte-compile: \"%s\" compiled successfully."
                (process-name process))
     (message "auto-byte-compile: \"%s\" %s"
              (process-name process)
@@ -43,10 +43,14 @@
 
 (defun auto-byte-compile-process-filter (process output)
   (when (buffer-live-p (process-buffer process))
-    (princ (format "%s%s\n\n"
-                   output
-                   (current-time-string))
-           (process-buffer process))))
+    (with-current-buffer (process-buffer process)
+      (goto-char (process-mark process))
+      (princ (format "%s\nauto byte compile: \"%s\"\n%s%s\n\n"
+                     (make-string 80 ?=)
+                     (process-name process)
+                     output
+                     (current-time-string))
+             (process-buffer process)))))
 
 (add-hook 'after-save-hook 'auto-byte-compile)
 
